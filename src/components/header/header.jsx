3d21b94432675d470f30b46da70d21195e9b3e4b
Jeremiah McDonald <45184971+XxJMcDoxX@users.noreply.github.com>
2022-11-19 12:02:14 -0600

    s
    s

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import './header.css'
-import {Navbar} from '../../components/nav/Nav'
+import Navbar from '../../components/nav/Nav'
 import { Parallax } from 'react-parallax';
 import dice from '../../assets/dice.jpg'
 
 const Header = () => {
   return (
-    <Parallax bgImage = {dice} strength= {200}>
-    <section className="header">
-      <Navbar />
-      <h5>Joe Locey</h5>
-      <h2>Dungeon master!</h2>
-    </section>
+    <Parallax bgImage={dice} strength={200}>
+      <section>
+        <header>
+          <div className="container header__container">
+            <h5>Joe Locey</h5>
+            <h2>Dungeon Master</h2>
+            <Navbar />
+          </div>
+        </header>
+      </section>
     </Parallax>
   )
 }
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './nav.css'
+import { IoLogoFacebook } from "react-icons/io";
+import { ImGithub } from "react-icons/im";
+import { BsSnapchat } from "react-icons/bs";
+import { AiOutlineLinkedin } from "react-icons/ai";
 
-const Navbar = () => {
+const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className="header__socials">
+    <a href= "https://www.linkedin.com" target="_blank" rel="noopener noreferrer"><AiOutlineLinkedin/></a>
+    <a href="https://github.com/XxJMcDoxX"  target="_blank" rel="noopener noreferrer"><ImGithub /></a>
+    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><IoLogoFacebook/></a>
+    <a href= "https://www.snapchat.com" target="_blank" rel="noopener noreferrer"><BsSnapchat /></a>
+
+    </div>
   )
 }
 
-export default Navbar
+export default Nav
