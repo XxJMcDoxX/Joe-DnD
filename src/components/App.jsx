bf6043aa422182ede0df75049c7e462be73370f9
Jeremiah McDonald <45184971+XxJMcDoxX@users.noreply.github.com>
2022-11-19 10:47:50 -0600

    a
    a

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Nav from './components/nav/Nav'
+import Login from './components/login/Login'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+    
+    <Nav />
+    <Login />
+    <Footer />
+    
+    </>
+  )
+}
+
+export default App
